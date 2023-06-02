6248ee487fe5471ae1238eea54912c87c0ce716b
NikhilThomas7 <Nikhil.Thomas@ust.com>
2023-06-02 16:43:09 +0530

    first commit

---
+++ src/main/java/com/ust/awspipe/resources/Connect.java
@@ -0,0 +1,18 @@
+package com.ust.awspipe.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
+public class Connect {
+	
+	@GetMapping
+	public String ready() {
+		return "1..2..3..gooo";
+	}
+	
+	
+
+}
---
+++ src/test/java/com/ust/awspipe/AwsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.awspipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
